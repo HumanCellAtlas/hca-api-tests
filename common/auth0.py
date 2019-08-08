43935b6f25a38603985b6bdc2bf2f50d3c9bcadb
Rolando Fernandez <rolando@ebi.ac.uk>
2019-08-08 16:12:51 +0100

    using service accounts

--- common/auth0.py
+++ common/auth0.py
@@ -1,44 +1,37 @@
-import os
+from ingest.utils.token_manager import TokenManager
+from ingest.utils.s2s_token_client import S2STokenClient
+from ingest.utils.dcp_auth_client import DCPAuthClient
 
 import logging
-import requests
 
-DEFAULT_AUTH0_DOMAIN = 'danielvaughan.eu.auth0.com'
-
-_auth_broker_url = os.environ.get('AUTH0_DOMAIN', DEFAULT_AUTH0_DOMAIN)
-_auth_broker_url = f'https://{_auth_broker_url}/oauth/token'
-
-_client_id = os.environ.get('CLIENT_ID', '')
-_client_secret = os.environ.get('CLIENT_SECRET', '')
-
-_sign_on_request = {
-    'client_id': _client_id,
-    'client_secret': _client_secret,
-    'audience': 'http://localhost:8080',
-    'grant_type': 'client_credentials'
-}
-
-
-def sign_on():
-    response = requests.post(_auth_broker_url, json=_sign_on_request)
-    return response.json()['access_token']
+DEFAULT_KEY_FILE_PATH = "/data/secret/key.json"
 
 
 class Authenticator:
 
-    def __init__(self):
+    def __init__(self, private_key_file_path):
         self._access_token = None
+        self.token_manager = self.setup_token_manager(private_key_file_path)
 
     def start_session(self):
         logging.info('Starting auth session...')
-        self._access_token = sign_on()
+        self._access_token = self.token_manager.get_token()
 
     def get_token(self):
-        if not self._access_token:
-            self.start_session()
-        return self._access_token
+        return self.token_manager.get_token()
 
     def end_session(self):
-        if self._access_token:
-            logging.info('Stopping auth session...')
-            self._access_token = None
+        logging.info('Stopping auth session...')
+
+    def setup_token_manager(self, private_key_file_path) -> TokenManager:
+        token_client = self.setup_from_file(private_key_file_path)
+        token_manager = TokenManager(token_client)
+        return token_manager
+
+    @staticmethod
+    def defaultAuthenticator():
+        '''
+        Initiate an authenticate from the default key file location
+        :return: an unconfigured Authenticator instance
+        '''
+        return Authenticator(DEFAULT_KEY_FILE_PATH)
