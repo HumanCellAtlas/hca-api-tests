275048455999f74885b99cab102e01b0ed32ffb3
Rolando Fernandez <rolando@ebi.ac.uk>
2019-08-09 11:15:30 +0100

    defaulting to port 8070 for the mock upload service

--- mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/MockUploadServiceApplication.java
+++ mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/MockUploadServiceApplication.java
@@ -2,10 +2,12 @@ package ingest.humancellatlas.org.mockuploadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@PropertySource("classpath:application.properties")
 public class MockUploadServiceApplication {
 
 	public static void main(String[] args) {
