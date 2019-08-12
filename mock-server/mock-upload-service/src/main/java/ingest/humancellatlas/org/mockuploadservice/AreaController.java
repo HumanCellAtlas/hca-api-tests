dbbe0aa5661887fdece1ad89b7431a02ac5c0376
Rolando Fernandez <rolando@ebi.ac.uk>
2019-08-12 14:41:53 +0100

    removed hardcoded API versioning

--- mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/AreaController.java
+++ mock-server/mock-upload-service/src/main/java/ingest/humancellatlas/org/mockuploadservice/AreaController.java
@@ -20,7 +20,7 @@ import static java.lang.String.format;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("v1/area")
+@RequestMapping("area")
 @AllArgsConstructor
 public class AreaController {
 
